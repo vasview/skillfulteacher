fb4de87218af1642eb582366bc9b4675a73ffe33
vasview <vasview@gmail.com>
2022-05-06 13:26:49 +0600

    Added media directory for uploaded files.

--- elklassproject/settings.py
+++ elklassproject/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Default root for media files uploaded by clients
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
