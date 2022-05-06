42f9cbfbceb80a8a87b363706e33ae2eb64f0186
vasview <vasview@gmail.com>
2022-05-06 13:27:34 +0600

    Registered some models in admin panel.

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,9 @@
+from ast import Sub
 from django.contrib import admin
+from school.models import City, Region, Nationality, Subject, JobPosition
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(Region)
+admin.site.register(Nationality)
+admin.site.register(Subject)
+admin.site.register(JobPosition)
